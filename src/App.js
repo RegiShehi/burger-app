63f5c9e9a16e9065bae5146a2c998beeaa056c1e
Regi <regi.shehi@softup.co>
2020-05-10 19:05:32 +0200

    Add routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Layout from "./hoc/Layout/Layout";
 import {Route, Switch} from 'react-router-dom';
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
+import Orders from "./containers/Orders/Orders";
 
 class App extends Component {
     render() {
@@ -11,7 +12,8 @@ class App extends Component {
                 <Layout>
                     <Switch>
                         <Route exact path='/' component={BurgerBuilder} />
-                        <Route exact path='/checkout' component={Checkout} />
+                        <Route exact path='/orders' component={Orders} />
+                        <Route path='/checkout' component={Checkout} />
                     </Switch>
                 </Layout>
             </div>
